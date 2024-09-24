9fbb86a9f4ee54c7ba3d58dd0d3ebf0967b26721
furefromex <ffb29272@gmail.com>
2024-09-24 19:48:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
         var currentIndex = 0;
 
         function openWebSocket() {
-            var url = 'wss://c74hber8wo.com/games-frame/sockets/crash?whence=22&fcountry=66&ref=1&gr=0&appGuid=games-web-master&lng=en&access_token=eyJhbGciOiJFUzI1NiIsImtpZCI6IjEiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI1MC85ODM4OTQyMzkiLCJwaWQiOiIxIiwianRpIjoiMC8xM2M3MGMzOTM1YzkyNjNjMDcwYzhmMGUzYTYxNzhmYzdjMGI1NmI5MTQ3MGQ0ODJiMDE4NWQ1NjdmOGM2ZjdjIiwiYXBwIjoiZmYzZDRhYmNmYmM2NWI0MF8yIiwieHBqIjoiMCIsInhnciI6IjAiLCJuYmYiOjE3MjcxODc1NTAsImV4cCI6MTcyNzE4ODc1MCwiaWF0IjoxNzI3MTg3NTUwfQ.K5r0wEatcdXx3fPy-LGe-CsmMCKLFa7-KRb1QfSP8FT4AHlJ-Oo6Yf8Xq1nh286cttAK_R0xtqvCeh_NP_K5AQ';
+            var url = 'wss://c74hber8wo.com/games-frame/sockets/crash?whence=22&fcountry=66&ref=1&gr=0&appGuid=games-web-master&lng=en&access_token=eyJhbGciOiJFUzI1NiIsImtpZCI6IjEiLCJ0eXAiOiJKV1QifQ.eyJzdWIiOiI1MC85ODM4OTQyMzkiLCJwaWQiOiIxIiwianRpIjoiMC9hN2FkNzlkMDk4NGZiZTMyNjQyMzFjY2VhNWU4YWE2OTZkMDkzYTU0YzE4MWVkZTI2YWQwZWE0ZTA4ZWE3ODJmIiwiYXBwIjoiZmYzZDRhYmNmYmM2NWI0MF8yIiwieHBqIjoiMCIsInhnciI6IjAiLCJuYmYiOjE3MjcyMDM2MDYsImV4cCI6MTcyNzIwNDgwNiwiaWF0IjoxNzI3MjAzNjA2fQ.km6sKFTze-atXv4TOe5aZ3YEH0IW6OaoocDi1WCWm-Xr7-5RmJejmjIVcIZsnGnA8CxqR6RGjgQXhO32oI_7eA';
             ws = new WebSocket(url);
             ws.onopen = function() {
                 console.log('WebSocket opened');
